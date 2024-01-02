61fd13041533bc5d3ccbc69b42536a5ecc3ac50d
dev-pengi <siffffdoul@gmail.com>
2024-01-02 13:09:12 +0100

    TODO: implement the stream url feature

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,6 +1,7 @@
 import { FC, useEffect } from "react";
 import { VideoPicker } from "../components";
 import { usePlayerContext } from "../contexts/PlayerContext";
+import { LinkIcon } from "../assets";
 
 const MainPage: FC = () => {
   const { setVideoFile } = usePlayerContext();
@@ -8,14 +9,30 @@ const MainPage: FC = () => {
     setVideoFile(null);
   }, []);
   return (
-    <div className="flex h-screen items-center">
-      <div className="flex-1 flex flex-col justify-center items-center">
-        <div className="backdrop-blur-2xl border-[1px] border-solid border-neutral-800 bg-zinc-800/30 from-inherit px-2 py-2">
-          <VideoPicker />
+    <div className="flex h-screen justify-center items-center">
+      <div className="backdrop-blur-2xl border-[1px] flex items-center w-full max-w-[1000px] rounded-md border-solid border-neutral-800 bg-zinc-800/30 from-inherit px-2 py-2">
+        <VideoPicker />
+        <div className="ml-6 flex-1">
+          <label htmlFor="media-url" className="text-white/50 uppercase">
+            Play with a URL
+          </label>
+          <div className="mt-2 relative">
+            <div className="text-[18px] absolute top-1/2 left-4 transform -translate-y-1/2">
+              <LinkIcon />
+            </div>
+            <input
+              type="text"
+              id="media-url"
+              className="px-6 py-3 pl-[46px] w-full h-max bg-transparent rounded-md border-neutral-600 border-solid border-[1px]"
+            />
+          </div>
+          <div className="flex">
+            <button className="py-2 px-9 mt-4 text-[14px] border-[2px] border-primary text-white bg-primary hover:opacity-80 duration-200 border-solid rounded-[4px]">
+              Play Video
+            </button>
+          </div>
         </div>
       </div>
-      <div className="w-[0.5px] h-full bg-gray-600" />
-      <div className="flex-1 px-3 flex flex-col justify-center items-center"></div>
     </div>
   );
 };
