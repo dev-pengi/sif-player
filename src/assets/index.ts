c07afd8c86f3f1fee8858d177d2687c6aeda2cbe
dev-pengi <siffffdoul@gmail.com>
2024-01-02 19:04:28 +0100

    minimize icon

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,6 +1,7 @@
 import PlayIcon from "./PlayIcon";
 import PauseIcon from "./PauseIcon";
 import MaximizeIcon from "./MaximizeIcon";
+import MinimizeIcon from "./MinimizeIcon";
 import MaxSoundIcon from "./MaxSoundIcon";
 import MinSoundIcon from "./MinSoundIcon";
 import MuteSoundIcon from "./MuteSoundIcon";
@@ -9,4 +10,4 @@ import PlaybackSpeedIcon from "./PlaybackSpeedIcon";
 import CheckIcon from './CheckIcon'
 import LinkIcon from "./LinkIcon";
 
-export { PlayIcon, PauseIcon, MaximizeIcon, MaxSoundIcon, MinSoundIcon, MuteSoundIcon, BackIcon, PlaybackSpeedIcon, CheckIcon, LinkIcon };
+export { PlayIcon, PauseIcon, MaximizeIcon, MinimizeIcon, MaxSoundIcon, MinSoundIcon, MuteSoundIcon, BackIcon, PlaybackSpeedIcon, CheckIcon, LinkIcon };
--- src/components/controllers/Buttons/FullScreenToggle.tsx
+++ src/components/controllers/Buttons/FullScreenToggle.tsx
@@ -1,17 +1,43 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
+import { motion, useAnimation } from "framer-motion";
+
 import Button from "./Button";
-import { MaximizeIcon } from "../../../assets";
+import { MaximizeIcon, MinimizeIcon } from "../../../assets";
 import { usePlayerContext } from "../../../contexts/PlayerContext";
 
-
 const FullScreenButton: FC = () => {
-  const { handleToggleScreen } = usePlayerContext();
+  const { handleToggleScreen, isFullScreen } = usePlayerContext();
+
+  const maximizeController = useAnimation();
+  const minimizeController = useAnimation();
+  const iconVariants = {
+    visible: { opacity: 1, scale: 1 },
+    hidden: { opacity: 0, scale: 0.2 },
+  };
+
+  useEffect(() => {
+    maximizeController.start(isFullScreen ? "hidden" : "visible");
+    minimizeController.start(isFullScreen ? "visible" : "hidden");
+  }, [isFullScreen]);
 
   return (
     <Button onClick={handleToggleScreen}>
-      <div className="absolute text-[24px] left-0 right-0 top-0 bottom-0 h-max w-max m-auto">
+      <motion.div
+        className="absolute left-0 right-0 top-0 bottom-0 h-max w-max m-auto text-[28px]"
+        variants={iconVariants}
+        initial="hidden"
+        animate={maximizeController}
+      >
         <MaximizeIcon />
-      </div>
+      </motion.div>
+      <motion.div
+        className="absolute text-[32px] left-0 right-0 top-0 bottom-0 h-max w-max m-auto"
+        variants={iconVariants}
+        initial="visible"
+        animate={minimizeController}
+      >
+        <MinimizeIcon />
+      </motion.div>
     </Button>
   );
 };
