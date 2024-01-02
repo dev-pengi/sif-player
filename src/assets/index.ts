4b8045a76aece4ec8a17f09a1b94a89ad6a27479
dev-pengi <siffffdoul@gmail.com>
2024-01-02 12:40:46 +0100

    chore: add new icons

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,5 +2,10 @@ import PlayIcon from "./PlayIcon";
 import PauseIcon from "./PauseIcon";
 import MaximizeIcon from "./MaximizeIcon";
 import MaxSoundIcon from "./MaxSoundIcon";
+import MinSoundIcon from "./MinSoundIcon";
+import MuteSoundIcon from "./MuteSoundIcon";
+import BackIcon from "./BackIcon";
+import PlaybackSpeedIcon from "./PlaybackSpeedIcon";
+import CheckIcon from './CheckIcon'
 
-export { PlayIcon, PauseIcon, MaximizeIcon, MaxSoundIcon }
+export { PlayIcon, PauseIcon, MaximizeIcon, MaxSoundIcon, MinSoundIcon, MuteSoundIcon, BackIcon, PlaybackSpeedIcon, CheckIcon }
