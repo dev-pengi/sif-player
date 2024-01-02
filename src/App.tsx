43763f765a28a7974c10f7147647105e237ea2dd
dev-pengi <siffffdoul@gmail.com>
2024-01-02 12:40:10 +0100

    import contexify css

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { MainPage, PlayerPage } from "./pages";
 import { PlayerContextProvider } from "./contexts/PlayerContext";
+import "react-contexify/dist/ReactContexify.css";
 
 function App() {
   return (
