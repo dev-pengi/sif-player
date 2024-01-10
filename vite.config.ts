5721abfb013766996ec629dbe37fda128c758cea
dev-pengi <siffffdoul@gmail.com>
2024-01-10 18:50:20 +0100

    Add baseUrl and paths in tsconfig.json, and resolve alias in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      //@ts-ignore
+      { find: '@', replacement: path.resolve(__dirname, 'src') },
+    ],
+  },
 })
